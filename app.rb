70c515cb421702f7bc7a8eee6f7fa440f45f1cc9
AshleeKaye <ashleemk@hotmail.com>
2018-06-12 21:27:27 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
-require_relative 'config/environment'
+require 'Sinatra'
 
 class App < Sinatra::Base
+  get '/' do 
+  "Hello World!"
+end
+  get '/name' do
+    "My name is Ashlee"
+  end
+  get '/hometown' do
+    "My home towns are Phoenix and Paradise Valley"
+  end
+  get '/song' do
+    "My favorite song is Bohemian Rhaspsody"
+  end
 end
