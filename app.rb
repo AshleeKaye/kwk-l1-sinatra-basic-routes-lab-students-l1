15f66de84ef23c10f9d556437ad2090c007ccb5d
AshleeKaye <ashleemk@hotmail.com>
2018-06-12 21:34:02 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
-require_relative 'config/environment'
+require 'sinatra'
 
 class App < Sinatra::Base
+  get '/' do 
+  "Hello World!"
+end
+  get '/name' do
+    "My name is Ashlee"
+  end
+  get '/hometown' do
+    "My hometown is Phoenix and Paradise Valley"
+  end
+  get '/favorite-song' do
+    "My favorite song is Bohemian Rhaspsody"
+  end
 end
